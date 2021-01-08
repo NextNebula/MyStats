d63fdb584c9a4c9011226597ea5f9691e1c1af56
Richard Leurs <7275740+EarthlingRich@users.noreply.github.com>
2021-01-07 22:25:48 -0400

    Initial commit .NET Core 5.0 Web Api project.

---
+++ MyStats/Controllers/MovieController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyStats.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private readonly ILogger<MovieController> _logger;
+
+        public MovieController(ILogger<MovieController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            var movies = new List<Movie>();
+            movies.Add(new Movie
+            {
+                Name = "James Bond"
+            });
+            return movies;
+        }
+    }
+}
---
+++ MyStats/Movie.cs
@@ -0,0 +1,7 @@
+namespace MyStats
+{
+    public class Movie
+    {
+        public string Name { get; set; }
+    }
+}
